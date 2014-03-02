e93998ac6fee703bd751bcaaada6f5a3185b7d80
LANE Boys RC <laneboysrc@gmail.com>
2014-03-02 20:18:04 +0800

    Algo improvement based on adjacent values have maximum difference

--- receiver-dump.py
+++ receiver-dump.py
@@ -13,16 +13,6 @@ import serial
 import sys
 import time
 
-jitterString0x07 = [
-    '|*  ..   |',
-    '| * ..   |',
-    '|  *..   |',
-    '|   *.   |',
-    '|   .*   |',
-    '|   ..*  |',
-    '|   .. * |',
-    '|   ..  *|'
-]
 
 jitterString0x0f = [
 #     0123456789abcdef
@@ -44,12 +34,61 @@ jitterString0x0f = [
     '|        .      *|'
 ]
 
+
 def int2bin(n, count=32):
-    """returns the binary of integer n, using count number of digits"""
-    chars = [' ', '1']
+    """Returns the binary of integer n, using count number of digits"""
+    chars = ['.', '1']
     return "".join([chars[(n >> y) & 1] for y in range(count-1, -1, -1)])
 
+
+oldData = 0
+valueCount = 0
+receivedValues = []
+expectedValues = [0x0f, 0x02, 0x00]
+startTime = time.time()
+
+def processValue(value):
+    global oldData
+    global valueCount
+    global receivedValues
+    global startTime
+    
+    data = value >> 4
+    
+    print "  0x%03x 0x%02x" % (value, data),
+    if oldData != data:
+        print "  %s" % (int2bin(data, 8), ),
+        if data == 0xa0:
+            now = time.time()
+            print "%3dms " % (int((now - startTime) * 1000), ),
+            startTime = now
+            if valueCount != 3:
+                print "  VALUECOUNT not 3: %d" % valueCount,
+            else:
+                error = False
+                for i, _ in enumerate(receivedValues):
+                    if i == 0:
+                        if expectedValues[i] != (receivedValues[i] & 0x3f):
+                            error = True
+                    else:
+                        if expectedValues[i] != (receivedValues[i] & 0x1f):
+                            error = True
+                if error:                    
+                    print "  DATA ERROR!",
+                    sys.exit(1)
+            valueCount = 0
+            receivedValues = []
+        else:
+            receivedValues.append(data)
+            valueCount = valueCount + 1
+            print "       ",
+    oldData = data
+
+
+largeChange = False
 def dump(port):
+    global largeChange
+    
     try:
         s = serial.Serial(port, 38400)
     except serial.SerialException, e:
@@ -57,10 +96,6 @@ def dump(port):
         sys.exit(0)
 
     oldValue = 0
-    oldData = 0
-    startTime = time.time()
-    bigDiff = False
-    glitch = " "
     
     diffs = []
     for _ in range(4):
@@ -80,18 +115,12 @@ def dump(port):
 
         jitter = value & 0xf
         print jitterString0x0f[jitter],
+        print "0x%03x" % (value, ),
 
-        if abs(oldValue - value) > 4:
-            #print  abs(oldValue - value),
-            if bigDiff or abs(oldValue - value) > 7:
-                oldValue = value
-                bigDiff = False
-                print "B ",
-            else:
-                bigDiff = True
-                print "G ",
-        else:
-            print "  ",
+        if abs(oldValue - value) <= 4:
+            largeChange = False
+            oldValue = value
+            print "      ",
 
             if value > 0x880:            
                 diffs.pop(0)
@@ -100,26 +129,33 @@ def dump(port):
                 for d in diffs:
                     avg += d
                 avg = (avg) / len(diffs)
-                print "avg=%3x " % (avg, ),
+                print "avg=%3x" % (avg, ),
             else:
-                print "        ",
-            
+                print "       ",
                         
-            data = value >> 4
-            print " 0x%03x 0x%02x" % (value, data),
-            if oldData != data:
-                now = time.time()
-                print "%3dms  %s" % (int((now - startTime) * 1000), int2bin(data, 8)),
-                print "  0x%02x" % (data, ),
-                startTime = now
-                glitch = " "
-
-
+            processValue(value)
+            
+        elif abs(oldValue - value) > 0x100:
+            if largeChange:
+                print "c*****",   
+            else:            
+                print "c     ",   
+            largeChange = True
+            if value > 0x880:
+                print "       ",
+                processValue(value)
             oldValue = value
-            oldData = data
+        else:
+            largeChange = False
+            if (oldValue < value):
+                value = oldValue
+            print "G>>>>>",
+                
+            print "       ",
+            processValue(value)
+            oldValue = value
 
         print
-
 
 
 if __name__ == '__main__':
