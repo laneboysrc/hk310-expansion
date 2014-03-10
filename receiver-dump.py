b8fe78739ea1c6ad5ca3dd961688832c0be451e8
Werner Lane <laneboysrc@gmail.com>
2014-03-10 11:54:20 +0800

    Code refactoring after moving from 7 to 6 significant bits

--- receiver-dump.py
+++ receiver-dump.py
@@ -13,9 +13,20 @@ import serial
 import sys
 import time
 
-TIMING_OFFSET = 0x20
+# Number of averages of the sync value. Used to check how good the clock
+# calibration in the PIC works
+AVERAGES = 4                
 
-jitterString0x0f = [
+TIMING_OFFSET = 0x20
+SYNC_VALUE_LIMIT = 0x880
+SYNC_VALUE_NOMINAL = 0xa40
+VALUE_COUNT = 3
+
+
+# This string allows us to visually inspect the lower 5 bits of the measured
+# pulse length, so we can see jitter and glitches.
+# The dot represents the desired target value we are aiming for.
+jitterString = [
 #     0123456789abcdef0123456789abcdef
     '|*       .                       |',
     '| *      .                       |',
@@ -48,7 +59,8 @@ jitterString0x0f = [
     '|        .                   *   |',
     '|        .                    *  |',
     '|        .                     * |',
-    '|        .                      *|'
+    '|        .                      *|',
+    '|        .                       |'    # Extra element containing just the boundaries.
 ]
 
 
@@ -58,107 +70,116 @@ def int2bin(n, count=32):
     return "".join([chars[(n >> y) & 1] for y in range(count-1, -1, -1)])
 
 
+def readInt(port):
+    """Read a decimal number, terminated by <LF>, from the given 
+       serial port. This function returns only when a valid decimal number
+       has been read.
+    """
+    while True:
+        c = port.read(1)
+        numString = ""
+        while c != "\n":
+            numString = numString + c
+            c = port.read(1)
+
+        try:        
+            return int(numString, 10)
+        except ValueError:
+            pass
+
+
+# "Static" variables for processValue()
 oldData = 0
-valueCount = 0
 receivedValues = []
-expectedValues = [0x0f, 0x00, 0x02]
 startTime = time.time()
 
 def processValue(value):
     global oldData
-    global valueCount
     global receivedValues
     global startTime
+
+    # Remove the offset we added in the transmitter to ensure the minimum
+    # pulse does not go down to zero.
+    if value < SYNC_VALUE_LIMIT:
+        value -= TIMING_OFFSET
+
+    # If we are dealing with a sync value we clamp it to the nominal value
+    # so that our check whether the value has changed does not trigger 
+    # wrongly when jitter moves between e.g. 0xa40 and 0xa3f
+    if value >= SYNC_VALUE_LIMIT:
+        value = SYNC_VALUE
     
+    # The actual usable data are only the upper 7 bits (bits 11..5) because of
+    # the jitter and glitches introduced by the receiver firmware    
     data = value >> 5
     
     print "  0x%03x 0x%02x" % (value, data),
+
     if oldData != data:
-        print "  %s" % (int2bin(data, 8), ),
-        if value > 0x880:
-            now = time.time()
-            print "%3dms " % (int((now - startTime) * 1000), ),
-            startTime = now
-            if valueCount != 3:
-                print "  VALUECOUNT not 3: %d" % valueCount,
-            else:
-                error = False
-                #for i, _ in enumerate(receivedValues):
-                #    if i == 0:
-                #        if expectedValues[i] != (receivedValues[i] & 0x3f):
-                #            error = True
-                #    else:
-                #        if expectedValues[i] != (receivedValues[i] & 0x1f):
-                #            error = True
-                if error:                    
-                    print "  DATA ERROR!",
-                    sys.exit(1)
-            valueCount = 0
-            receivedValues = []
-        else:
+        if value < SYNC_VALUE_LIMIT:
             receivedValues.append(data)
             valueCount = valueCount + 1
-            print "       ",
+        else:        
+            now = time.time()
+            print "  %3dms" % (int((now - startTime) * 1000), ),
+            startTime = now
+            
+            if len(receivedValues) != VALUE_COUNT:
+                print "VALUECOUNT IS %d, SHOULD BE %d" % (len(receivedValues), VALUE_COUNT),
+            else:
+                # Remove bit 5 which we use to force large boundaries between
+                # values
+                for i in range(1, len(receivedValues)):
+                    receivedValues[i] = receivedValues[i] & 0x1f
+
+                for d in receivedValues:           
+                    print "0x%02x" % d,
+                
+                print "   "    
+
+                numBits = 6
+                for d in receivedValues:           
+                    print int2bin(d, numBits), " "
+                    numBits = 5
+                    
+            receivedValues = []
     oldData = data
 
 
-largeChange = False
 def dump(port):
-    global largeChange
-    
     try:
         s = serial.Serial(port, 38400)
     except serial.SerialException, e:
         print("Unable to open port %s.\nError message: %s" % (port, e))
         sys.exit(0)
 
-    oldValue = 0
-    
-    diffs = []
-    for _ in range(4):
-        diffs.append(0xa40)
+    diffs = [SYNC_VALUE_NOMINAL] * AVERAGES
     
     while True:
-        c = s.read(1)
-        numString = ""
-        while c != "\n":
-            numString = numString + c
-            c = s.read(1)
-
-        try:        
-            value = int(numString, 10)
-        except ValueError:
-            continue
+        value = readInt(s)
     
-        if value < 0x880:            
-            # Show the jitter diagram (lower 4 bits)
-            jitter = value & 0x1f
-            print jitterString0x0f[jitter],
+        # Show the jitter diagram
+        if value < SYNC_VALUE_LIMIT:            
+            print jitterString[value & 0x1f],
         else:
-            print '|        .                       |',
+            print jitterString[-1],
 
+        # Print the raw value we read from the receiver
         print "0x%03x" % (value, ),
-
-        # Remove the offset we added in the transmitter to ensure the minimum
-        # pulse does not go down to zero.
-        if value < 0x880:            
-            value -= TIMING_OFFSET
-
         
         # Calculate a rolling average of the last n sync pulses
-        if value > 0x880:            
+        if value >= SYNC_VALUE_LIMIT:            
             diffs.pop(0)
             diffs.append(value)
             avg = 0
             for d in diffs:
                 avg += d
             avg = (avg) / len(diffs)
-            print "avg=%3x" % (avg, ),
+            print "avg=0x%3x" % (avg, ),
         else:
-            print "       ",
+            print "         ",
                     
         processValue(value)
-
         print
 
 
@@ -171,5 +192,6 @@ if __name__ == '__main__':
     try:
         dump(port)
     except KeyboardInterrupt:
-        print ""
+        print
         sys.exit(0)
+        
